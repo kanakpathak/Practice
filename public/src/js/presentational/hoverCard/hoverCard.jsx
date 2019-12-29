4f7968b6fbd02cc31d26a58427f9f8ec349a0707
kanakpathak <kanakpathak1@gmail.com>
2019-12-29 19:24:46 +0530

    hover card changes

--- public/src/js/presentational/hoverCard/hoverCard.jsx
+++ public/src/js/presentational/hoverCard/hoverCard.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import styles from "./hoverCard.css";
 
 const HoverCard = () => {
-  const arr = [1, 2, 3, 4];
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
-    <div className={wrapper}>
+    <div className={styles.wrapper}>
       {arr.map((index, value) => {
         return (
-          <img
-            className={styles.card}
-            key={index}
-            src="https://www.ozanoo.com/uploads/ozanoo-jewelery/products/se10013-1-749357_l.jpg"
-            height="25%"
-            width="20%"
-          />
+          <div key={index} className={styles.card}>
+            <img
+              className={styles.cardImg}
+              src="https://www.ozanoo.com/uploads/ozanoo-jewelery/products/se10013-1-749357_l.jpg"
+            />
+            <div className={styles.content}>
+              <h1 style={{ color: "white" }}>I m overlapping text</h1>
+            </div>
+          </div>
         );
       })}
     </div>
